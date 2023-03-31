d10cc717babce0fa17ebafcf9d96f14d58866300
baolunkk710 <thanhbaohuynh710@gmail.com>
2023-03-31 22:22:27 +0700

    first commit

---
+++ video3/video3.js
@@ -0,0 +1,20 @@
+
+
+// let c = 5;
+// let d = 5.9;
+
+// const x = 5;
+
+// x = 5;
+// window.onload() = function()
+
+console.log(`Hello from JS:`)
+
+
+let x = 5, y = 6, z = 7;
+console.log("Test String", 'x=', x,'y= ', y,'z= ', z);
+console.log("Test String 2222:", + 'x=' + x +'y=' +y + 'z=' +z)
+
+
+
+
